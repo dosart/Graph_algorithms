63c29dd8456afac9ac5d8627054aee1f692e7940
Denis <Dysprosium92@ya.ru>
2021-03-26 23:01:47 +0300

    Added exeption for graph

---
+++ algoritms/exception/graph_exception.py
@@ -0,0 +1,19 @@
+"""Exceptions of graphs."""
+
+
+class GraphContainsVertexExemption(Exception):
+    """Exception class if a graph contains vertex."""
+
+    pass  # noqa: WPS420, WPS604
+
+
+class GraphIsEmptyException(Exception):
+    """Exception class if a graph is empty."""
+
+    pass  # noqa: WPS420, WPS604
+
+
+class GraphNotContainsVertexException(Exception):
+    """Exception class if a graph contains vertex."""
+
+    pass  # noqa: WPS420, WPS604
