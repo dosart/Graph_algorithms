94b836b3336ac38dd20dfd98cd978d02aa0c7221
dosart <you@example.com>
2021-03-31 08:54:55 +0300

    Added dfs algorithm (iterative version)

---
+++ algoritms/dfs_iterative.py
@@ -0,0 +1,24 @@
+"""Implementation iterative depth-first search."""
+
+
+def dfs_iterative(graph):
+    """Visit all the vertex of graph.
+
+    Args:
+        graph: graph for search
+
+    Returns:
+        visited (dict): key (vertex id), value (true if vetrex is visited)
+    """
+    visited = {vertex : False for vertex in graph}
+
+    stack = []
+    for vertex in graph:
+        stack.append(vertex)
+
+    while len(stack) > 0:
+        vertex = stack.pop()
+        for adjacent_vertex in vertex:
+            if visited[adjacent_vertex] is False:
+                  stack.append(adjacent_vertex)
+                  visited[adjacent_vertex] == True
