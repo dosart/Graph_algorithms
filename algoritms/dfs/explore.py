ddff2ff99c2512f3b401be76a592e7c54c0e0f4f
Denis <Dysprosium92@ya.ru>
2021-04-03 10:07:39 +0300

    Move file explore.py in folder dfs

---
+++ algoritms/dfs/explore.py
@@ -0,0 +1,21 @@
+"""Algorithm for visiting vertices of a connected component."""
+
+
+def explore(vertex, visited, pre_visit, post_visit):
+    """Find all vertices reachable from a arg.
+
+    Args:
+        vertex: start vertex for explore
+        visited (dict): key (vertex id), value (true if vetrex is visited)
+        pre_visit (function): сalled before vertex processing
+        post_visit (function): сalled after vertex processing
+    """
+    visited[vertex.identifier] = True
+
+    pre_visit(vertex)
+
+    for adjacent_vertex in vertex:
+        if visited[adjacent_vertex.identifier] is False:
+            explore(adjacent_vertex, visited, pre_visit, post_visit)
+
+    post_visit(vertex)
