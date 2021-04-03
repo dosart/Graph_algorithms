d6e75fcefb511d43c18d7f8dff75ec595c3095d3
Denis <Dysprosium92@ya.ru>
2021-04-03 10:06:24 +0300

    Added the algorithm for calculating the components of graph connectivity

---
+++ algoritms/dfs/connected_components.py
@@ -0,0 +1,68 @@
+"""Implementation of the algorithm for calculating the components of graph connectivity."""
+
+from algoritms.dfs.explore import explore
+
+
+class _ConnectedComponents(object):
+    """The class counts the connected components of the graph."""
+
+    def __init__(self, count=1):
+        """Construct a new connected components data structure.
+
+        Args:
+            count (int): the initial value of the counter
+        """
+        self._components = {}
+        self._count = count
+
+    @property
+    def components(self):
+        """Return the number connected components.
+
+        Returns:
+            components (dict): key (vertex id), value (connectivity component number)
+        """
+        return self._components
+
+    def inc_count(self):
+        """Increment count the connected components."""
+        self._count += 1
+
+    def pre_visit(self, vertex):
+        """Set number of the connected component for vertex.
+
+        Function called in explore function
+
+        Args:
+            vertex: vertex of graph
+        """
+        self._components[vertex.identifier] = self._count
+
+    def post_visit(self, vertex):
+        """Doing nothing.
+
+        Function called in explore function
+
+        Args:
+            vertex: vertex of graph
+        """
+        pass
+
+
+def connected_components(graph):
+    """Return the number connected components.
+
+       Args:
+            graph: graph for calculating connectivity components
+
+       Returns:
+            components (dict): key (vertex id), value (connectivity component number)
+    """
+    visited = {vertex.identifier: False for vertex in graph}
+    components = _ConnectedComponents()
+
+    for vertex in graph:
+        if visited[vertex.identifier] is False:
+            explore(vertex, visited, components.pre_visit, components.post_visit)
+            components.inc_count()
+    return components.components
---
+++ tests/test_connected_components.py
@@ -0,0 +1,58 @@
+"""Tests of algorithm for calculating the components of graph connectivity."""
+
+import pytest
+
+from algoritms.graph import Graph
+from algoritms.dfs.connected_components import connected_components
+
+
+def test_one_component():
+    graph = Graph()
+
+    graph.create_vertex_by_id('A')
+    graph.create_vertex_by_id('B')
+    graph.create_vertex_by_id('C')
+
+    graph.add_edge('A', 'B')
+    graph.add_edge('B', 'C')
+
+    components = connected_components(graph)
+
+    assert components['A'] == 1
+    assert components['B'] == 1
+    assert components['C'] == 1
+
+
+def test_two_component():
+    graph = Graph()
+
+    graph.create_vertex_by_id('A')
+    graph.create_vertex_by_id('B')
+    graph.create_vertex_by_id('C')
+    graph.create_vertex_by_id('D')
+
+    graph.add_edge('A', 'B')
+    graph.add_edge('C', 'D')
+
+    components = connected_components(graph)
+
+    assert components['A'] == 1
+    assert components['B'] == 1
+    assert components['C'] == 2
+    assert components['D'] == 2
+
+
+def test_four_component():
+    graph = Graph()
+
+    graph.create_vertex_by_id('A')
+    graph.create_vertex_by_id('B')
+    graph.create_vertex_by_id('C')
+    graph.create_vertex_by_id('D')
+
+    components = connected_components(graph)
+
+    assert components['A'] == 1
+    assert components['B'] == 2
+    assert components['C'] == 3
+    assert components['D'] == 4
