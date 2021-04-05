aa7d75349fd7752c8fad3d30f1bc3f41db86d85a
Denis <Dysprosium92@ya.ru>
2021-04-05 19:12:47 +0300

    function stab

---
+++ algoritms/dfs/contains_circle.py
@@ -0,0 +1,13 @@
+"""The implementation of the verification algorithm contains a cycle graph using DFS."""
+
+
+def contains_circle(graph):
+    """Return only false.
+
+    Args:
+        graph: graph for check
+
+    Returns:
+        result (bool): false if graph not contains circle
+    """
+    return False
