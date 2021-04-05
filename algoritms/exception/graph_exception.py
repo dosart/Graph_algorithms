d5950214cc4045900d4bbaacce3dc98eb1324b2f
Denis <Dysprosium92@ya.ru>
2021-04-05 19:11:35 +0300

    Exeptions for topological sort

--- algoritms/exception/graph_exception.py
+++ algoritms/exception/graph_exception.py
@@ -17,3 +17,27 @@ class GraphNotContainsVertexException(Exception):
     """Exception class if a graph contains vertex."""
 
     pass  # noqa: WPS420, WPS604
+
+
+class GraphContainsCircleException(Exception):
+    """Exception class if a graph contains circle."""
+
+    pass  # noqa: WPS420, WPS604
+
+
+def contains_circle_message():
+    """Return message for GraphContainsCircleException.
+
+    Returns:
+        message (str) message for exeption
+    """
+    return 'Graph contains circle.'
+
+
+def sorting_not_possible_message():
+    """Return message for topological sort.
+
+    Returns:
+        message (str) message for exeption
+    """
+    return 'Sorting is not possible. Graph contains circle.'
