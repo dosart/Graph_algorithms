496d91536ce8fedc1ab3521f293846f73fbee1c0
Denis <Dysprosium92@ya.ru>
2021-04-05 19:09:28 +0300

    Function signature changed. Function takes functions pre visit and post visit

--- algoritms/dfs/dfs_recursion.py
+++ algoritms/dfs/dfs_recursion.py
@@ -3,33 +3,13 @@
 from algoritms.dfs.explore import explore
 
 
-def _pre_visit(vertex):
-    """Doing nothing.
-
-       Function called in explore function
-
-       Args:
-            vertex: vertex of graph
-    """
-    pass
-
-
-def _post_visit(vertex):
-    """Doing nothing.
-
-       Function called in explore function
-
-       Args:
-            vertex: vertex of graph
-    """
-    pass
-
-
-def dfs_recursion(graph):
+def dfs_recursion(graph, pre_visit, post_visit):
     """Visit all the vertex of graph.
 
     Args:
         graph: graph for search
+        pre_visit (function): сalled before vertex processing
+        post_visit (function): сalled after vertex processing
 
     Returns:
         visited (dict): key (vertex id), value (true if vetrex is visited)
@@ -38,5 +18,5 @@ def dfs_recursion(graph):
 
     for vertex in graph:
         if visited[vertex.identifier] is False:
-            explore(vertex, visited, _pre_visit, _post_visit)
+            explore(vertex, visited, pre_visit, post_visit)
     return visited
