80c098a8ff901689d0c7cdb9cc1028de1565afce
Denis <Dysprosium92@ya.ru>
2021-04-09 23:31:51 +0300

    Removed distance collection functions

--- algorithms/paths_in_graph/bfs/bfs.py
+++ algorithms/paths_in_graph/bfs/bfs.py
@@ -1,43 +1,50 @@
 """Implementation iterative breadth-first search."""
 
-from collections import deque
+from algorithms.exception.graph_exception import GraphIsEmptyException
+from algorithms.exception.messages import graph_is_empty_message
 
+from algorithms.exception.graph_exception import GraphNotContainsVertexException
+from algorithms.exception.messages import graph_not_contains_vertex_message
 
-def make_distances(graph):
-    return {vertex: None for vertex in graph}
-
-
-def set_distance(vertex, distance, distances):
-    distances[vertex] = distance
-
-
-def get_distance(vertex, distances):
-    return distances[vertex]
-
-
-def not_visited(vertex, distances):
-    return True if distances[vertex] is None else False
-
-
-def make_queue():
-    return deque()
-
-
-def enqueue(queue, element):
-    queue.append(element)
-
-
-def dequeue(queue):
-    return queue.popleft()
-
-
-def is_empty(queue):
-    return len(queue) == 0
+from algorithms.paths_in_graph.bfs.queue import make_queue, enqueue, dequeue, is_empty
+from algorithms.paths_in_graph.distance import get_distance, make_distances, not_visited, set_distance
 
 
 def bfs(graph, start_vertex):
+    """Return the distance from the starting vertex to the rest.
+
+    Args:
+        graph: graph for search
+        start_vertex: start vertex for search
+
+    Raises:
+        GraphIsEmptyException: if graph is emoty
+        GraphNotContainsVertexException: if graph not contains start vertex
+
+    Returns:
+        distances: collection storing the distance from the starting vertex to the rest
+
+    """
+    if graph.is_empty:
+        raise GraphIsEmptyException(graph_is_empty_message())
+    if start_vertex.identifier not in graph:
+        raise GraphNotContainsVertexException(graph_not_contains_vertex_message(start_vertex.identifier))
+    return _bfs(graph, start_vertex)
+
+
+def _bfs(graph, start_vertex):
+    """Return the distance from the starting vertex to the rest.
+
+    Args:
+        graph: graph for search
+        start_vertex: start vertex for search
+
+    Returns:
+        distances: collection storing the distance from the starting vertex to the rest
+
+    """
     distances = make_distances(graph)
-    set_distance(start_vertex, 0)
+    set_distance(start_vertex, 0, distances)
 
     vertices = make_queue()
     enqueue(vertices, start_vertex)
@@ -47,6 +54,6 @@ def bfs(graph, start_vertex):
         for adjacent_vertex in vertex:
             if not_visited(adjacent_vertex, distances):
                 enqueue(vertices, adjacent_vertex)
-                set_distance(adjacent_vertex, distances,
-                             get_distance(vertex) + 1)
+                distance = get_distance(vertex, distances)
+                set_distance(adjacent_vertex, distance + 1, distances)
     return distances
