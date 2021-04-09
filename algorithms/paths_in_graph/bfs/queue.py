1ffe0b5a8681b7479268a25160b604f57983ea0f
Denis <Dysprosium92@ya.ru>
2021-04-09 23:22:09 +0300

    Added simple queue for bfs

---
+++ algorithms/paths_in_graph/bfs/queue.py
@@ -0,0 +1,49 @@
+"""Implementation simple queue for breadth-first search.
+
+Private data structure. Only for BFS.
+"""
+
+from collections import deque
+
+
+def make_queue():
+    """Make and return queue.
+
+    Returns:
+        queue: simple queue
+    """
+    return deque()
+
+
+def enqueue(queue, element):
+    """Add element in queue.
+
+    Args:
+        queue: simple queue
+        element: element to added
+    """
+    queue.append(element)
+
+
+def dequeue(queue):
+    """Return first element from queue.
+
+    Args:
+        queue: simple queue
+
+    Returns:
+        element: first element from queue
+    """
+    return queue.popleft()
+
+
+def is_empty(queue):
+    """Return true if queue is empty.
+
+    Args:
+        queue: simple queue
+
+    Returns:
+        result (bool): true if queue is empty
+    """
+    return len(queue) == 0
