78903fdcf3d98c16d3675439a3ba97643749f216
Denis <Dysprosium92@ya.ru>
2021-04-11 17:17:30 +0300

    Added dijkstra's algorithms

---
+++ algorithms/paths_in_graph/dijkstra/dijkstra.py
@@ -0,0 +1,66 @@
+"""Implementation Dijkstra’s algorithm."""
+
+from algorithms.paths_in_graph.priority_queue.priority_queue import make_priority_queue
+from algorithms.paths_in_graph.priority_queue.priority_queue import extract_min
+from algorithms.paths_in_graph.priority_queue.priority_queue import set_priority
+from algorithms.paths_in_graph.priority_queue.priority_queue import decrease_priority
+from algorithms.paths_in_graph.priority_queue.priority_queue import is_empty
+
+from algorithms.paths_in_graph.distance import make_distances
+from algorithms.paths_in_graph.distance import get_distance
+from algorithms.paths_in_graph.distance import set_distance
+from algorithms.paths_in_graph.distance import to_int
+
+from algorithms.exception.graph_exception import GraphNotContainsVertexException
+from algorithms.exception.messages import graph_not_contains_vertex_message
+
+from algorithms.exception.graph_exception import GraphIsEmptyException
+from algorithms.exception.messages import graph_is_empty_message
+
+
+def dijkstra(graph, start_vertex):
+    """Return the distance from the starting vertex to the rest.
+
+    Args:
+        graph: graph for search
+        start_vertex: start vertex for search
+
+    Raises:
+        GraphIsEmptyException: if graph is emoty
+        GraphNotContainsVertexException: if graph not contains start vertex
+
+    Returns:
+        distances: collection storing the distance from the starting vertex to the rest
+    """
+    if graph.is_empty:
+        raise GraphIsEmptyException(graph_is_empty_message())
+    if start_vertex.identifier not in graph:
+        raise GraphNotContainsVertexException(graph_not_contains_vertex_message(start_vertex.identifier))
+    return _dijkstra(graph, start_vertex)
+
+
+def _dijkstra(graph, start_vertex):
+    """Return the distance from the starting vertex to the rest.
+
+    Args:
+        graph: graph for search
+        start_vertex: start vertex for search
+
+    Returns:
+        distances: collection storing the distance from the starting vertex to the rest
+
+    """
+    distance = make_distances(graph)
+    priority_queue = make_priority_queue(graph)
+
+    set_distance(start_vertex, 0, distance)
+    set_priority(start_vertex, 0, priority_queue)
+
+    while not is_empty(priority_queue):
+        vertex = extract_min(priority_queue)
+        for adjacent_vertex in vertex:
+            weight = to_int(get_distance(vertex, distance)) + vertex.edge_weight_to(adjacent_vertex)
+            if to_int(get_distance(adjacent_vertex, distance)) > weight:
+                set_distance(adjacent_vertex, weight, distance)
+                decrease_priority(adjacent_vertex, weight, priority_queue)
+    return distance
---
+++ tests/paths_in_graph/test_dijkstra.py
@@ -0,0 +1,94 @@
+import pytest
+
+from algorithms.graph.graph import Graph
+from algorithms.graph.vertex import Vertex
+
+from algorithms.paths_in_graph.dijkstra.dijkstra import dijkstra
+
+from algorithms.paths_in_graph.distance import get_distance
+
+from algorithms.exception.graph_exception import GraphNotContainsVertexException
+from algorithms.exception.messages import graph_not_contains_vertex_message
+
+from algorithms.exception.graph_exception import GraphIsEmptyException
+from algorithms.exception.messages import graph_is_empty_message
+
+
+def test_dijkstra_positive1():
+    graph = Graph()
+
+    graph.create_vertex_by_id('A')
+    graph.create_vertex_by_id('B')
+    graph.create_vertex_by_id('C')
+    graph.create_vertex_by_id('D')
+    graph.create_vertex_by_id('E')
+
+    graph.add_edge('A', 'C', 2)
+    graph.add_edge('A', 'B', 4)
+
+    graph.add_edge('B', 'C', 3)
+    graph.add_edge('B', 'D', 2)
+    graph.add_edge('B', 'E', 3)
+
+    graph.add_edge('C', 'B', 1)
+    graph.add_edge('C', 'D', 4)
+    graph.add_edge('C', 'E', 5)
+
+    graph.add_edge('E', 'D', 1)
+
+    distance = dijkstra(graph, graph.get_vertex_by_id('A'))
+
+    distance_to_C = get_distance(graph.get_vertex_by_id('C'), distance)
+    assert distance_to_C == 2
+
+    distance_to_B = get_distance(graph.get_vertex_by_id('B'), distance)
+    assert distance_to_B == 3
+
+    distance_to_E = get_distance(graph.get_vertex_by_id('E'), distance)
+    assert distance_to_E == 6
+
+    distance_to_D = get_distance(graph.get_vertex_by_id('D'), distance)
+    assert distance_to_D == 5
+
+
+def test_dijkstra_positive2():
+    graph = Graph()
+
+    graph.create_vertex_by_id('A')
+    graph.create_vertex_by_id('B')
+    graph.create_vertex_by_id('C')
+    graph.create_vertex_by_id('D')
+
+    graph.add_edge('A', 'C', 2)
+    graph.add_edge('A', 'B', 4)
+
+    distance = dijkstra(graph, graph.get_vertex_by_id('A'))
+
+    distance_to_B = get_distance(graph.get_vertex_by_id('B'), distance)
+    assert distance_to_B == 4
+
+    distance_to_C = get_distance(graph.get_vertex_by_id('C'), distance)
+    assert distance_to_C == 2
+
+    distance_to_D = get_distance(graph.get_vertex_by_id('D'), distance)
+    assert distance_to_D is None
+
+
+def test_dijkstra_negative1():
+    graph = Graph()
+
+    with pytest.raises(GraphIsEmptyException) as exception_info:
+        dijkstra(graph, Vertex('A'))
+
+    assert str(exception_info.value) == graph_is_empty_message()
+
+
+def test_dijkstra_negative2():
+    graph = Graph()
+
+    graph.create_vertex_by_id('A')
+
+    with pytest.raises(GraphNotContainsVertexException) as exception_info:
+        dijkstra(graph, Vertex('B'))
+
+    assert str(exception_info.value) == graph_not_contains_vertex_message('B')
